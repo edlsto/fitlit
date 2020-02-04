a759e2a74091028212f3f0675bf68558513e2502
Edward Stoner <edwardlstoner@gmail.com>
2020-02-04 15:16:22 -0700

    Create User Repository class

---
+++ src/UserRepository.js
@@ -0,0 +1,24 @@
+const data = require('../data/users-sample.js');
+
+class UserRepository {
+  constructor(data) {
+    this.data = data;
+  }
+
+  getUserData(id) {
+    return this.data.find(user => user.id === id);
+  }
+
+  getAverageStepGoal() {
+    return this.data.reduce(user => (acc, el) => {
+      acc += el.dailyStepGoal;
+      return acc;
+    }, 0) / this.data.length;
+  }
+
+
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = UserRepository;
+}
