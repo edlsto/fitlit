f0ec682aaeb0a1babcd10fd665923d229cea7429
Edward Stoner <edwardlstoner@gmail.com>
2020-02-05 16:59:11 -0700

    Display hydration data

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,5 @@
-let currentUser = new User(userData[0]);
+let currentUser = new User(userData[3]);
+let hydration = new Hydration(hydrationData)
 let userRepository = new UserRepository(userData);
 let username = document.querySelector("#name");
 let userAddress = document.querySelector("#address");
@@ -7,6 +8,8 @@ let userStrideLength = document.querySelector("#stride-length");
 let userStepGoal = document.querySelector("#daily-step-goal");
 let averageStepGoal = document.querySelector("#average-step-goal");
 let stepCompare = document.querySelector("#step-compare");
+let waterToday = document.querySelector("#water-today")
+let waterWeekList = document.querySelector('#water-week-list')
 // let userFriends = document.querySelector("#friends");
 
 username.innerText = currentUser.returnFirstName();
@@ -16,6 +19,11 @@ userStrideLength.innerText = currentUser.strideLength;
 userStepGoal.innerText = currentUser.dailyStepGoal;
 // userFriends.innerText = currentUser.friends;
 
-console.log(userRepository.getAverageStepGoal());
 averageStepGoal.innerText = userRepository.getAverageStepGoal();
 stepCompare.innerText = currentUser.dailyStepGoal > userRepository.getAverageStepGoal() ? 'higher' : 'lower';
+let today = '2019/08/15'
+waterToday.innerText = hydration.waterConsumedForSpecificDay(currentUser.id, today)
+let waterWeek = hydration.waterConsumedForSpecificWeek(currentUser.id, today);
+waterWeek.forEach(el => {
+  waterWeekList.insertAdjacentHTML('afterbegin', `<li>${el} ounces</ul>`)
+});
