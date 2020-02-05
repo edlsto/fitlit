73a585bb6695a6bc8e4911812dada4f1ee2bca8e
Edward Stoner <edwardlstoner@gmail.com>
2020-02-05 16:15:26 -0700

    Modify test

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -3,6 +3,7 @@ const expect = chai.expect;
 
 const Hydration = require('../src/Hydration')
 const data = require('../data/hydration-sample.js');
+const fullData = require('../data/hydration.js');
 
 describe('Hydration', function() {
 
@@ -23,6 +24,11 @@ describe('Hydration', function() {
   it('should show water consumed for a specific day', function() {
     const hydration = new Hydration(data);
     expect(hydration.waterConsumedForSpecificDay(1, '2019/06/16')).to.equal(42);
+  })
+
+  it('should show water consumed each day for specified last seven days', function() {
+    const hydration = new Hydration(fullData);
+    expect(hydration.waterConsumedForSpecificWeek(3, '2019/09/18')).to.deep.equal([32, 69, 22, 58, 87, 38, 64]);
   })
 
 });
