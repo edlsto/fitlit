0e956772502bd3a551a328c9f1d67b9c640cb68c
Lane Hunter <lanehunter.96@gmail.com>
2020-02-06 14:44:42 -0700

    Add findGoodSleepers function

--- src/Sleep.js
+++ src/Sleep.js
@@ -11,7 +11,7 @@ class Sleep {
       return acc;
     }, 0) / userSleepData.length
   }
-  averageSleepQuality(userID) {
+  averageSleepQualityForUser(userID) {
     let userSleepData = this.sleepData.filter(el => {
       return el.userID === userID;
     });
@@ -45,13 +45,36 @@ class Sleep {
       return (elementDate <= date) && (elementDate > sevenDaysAgo)
       && el.userID === userID
     });
-    console.log(filteredResults);
     return filteredResults.map(el => {
       return el.sleepQuality
     });
   }
-  findGoodSleepers(week) {
-
+  findGoodSleepers(endDate) {
+    const date = new Date(endDate)
+    const sevenDaysAgo = new Date(new Date(endDate).setDate(new Date(endDate).getDate() - 7))
+    var filteredResults = this.sleepData.filter(el => {
+      const elementDate = new Date(el.date)
+      return (elementDate <= date) && (elementDate > sevenDaysAgo)
+    });
+      let userList = filteredResults.reduce((acc, el) => {
+        if (!acc.includes(el.userID)) {
+          acc.push(el.userID)
+        }
+        return acc
+      }, [])
+      let sleepQualityAverages = userList.map(el => {
+        return {
+          userID: el,
+          averageSleepQuality: Number((filteredResults.reduce((acc, result) => {
+            if (result.userID === el) {
+              acc += result.sleepQuality;
+            }
+            return acc;
+          }, 0) / 7).toFixed(1))
+        }
+      })
+      console.log(sleepQualityAverages.filter(user => user.averageSleepQuality > 3));
+      return sleepQualityAverages.filter(user => user.averageSleepQuality > 3)
   }
   findLongestSleepers(date) {
 
