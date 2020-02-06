7ca0ce0d0dc81a52002a28a74a4cb99d1b3f4e6e
Lane Hunter <lanehunter.96@gmail.com>
2020-02-06 12:54:26 -0700

    Add testing for sleepQualityForSpecificWeek function

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -26,10 +26,20 @@ describe('Sleep', function() {
     expect(sleep.averageSleepQuality(1)).to.equal(2.9)
   });
 
-  it('should find the hours slept for a specific day', function() {
+  it('should return hours slept for a specific day', function() {
     const sleep = new Sleep(data);
     expect(sleep.getHoursSlept(3,"2019/06/15")).to.equal(10.8)
     expect(sleep.getHoursSlept(2,"2019/06/15")).to.equal(7)
-  })
+  });
+
+  it('should find the hours slept for a specific week', function() {
+    const sleep = new Sleep(fullData);
+    expect(sleep.hoursSleptForSpecificWeek(22,"2019/09/22")).to.deep.equal([ 9.3, 9.2, 9.5, 7.5, 5.1, 11, 6 ])
+  });
+
+  it('should return a user\'s sleep quality for a specific week', function() {
+    const sleep = new Sleep(fullData);
+    expect(sleep.sleepQualityForSpecificWeek(22,"2019/09/22")).to.deep.equal([ 4, 4.8, 4.7, 4.6, 1.4, 1.7, 3.2 ])
+  });
 
 });
