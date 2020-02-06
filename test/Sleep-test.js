ca522dfa35276ad065249624b2d2b870b24d42da
Lane Hunter <lanehunter.96@gmail.com>
2020-02-05 22:49:09 -0700

    Add testing for getHoursSlept method

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -16,11 +16,20 @@ describe('Sleep', function() {
     expect(sleep).to.be.an.instanceof(Sleep);
   });
 
-  it('should calculate average water consumed per day', function() {
+  it('should calculate average hours of sleep', function() {
     const sleep = new Sleep(data);
     expect(sleep.averageHoursSleptPerDay(1)).to.equal(5.1);
   });
 
-  it('should')
+  it('should calculate average sleep quality', function() {
+    const sleep = new Sleep(data);
+    expect(sleep.averageSleepQuality(1)).to.equal(2.9)
+  });
+
+  it('should find the hours slept for a specific day', function() {
+    const sleep = new Sleep(data);
+    expect(sleep.getHoursSlept(3,"2019/06/15")).to.equal(10.8)
+    expect(sleep.getHoursSlept(2,"2019/06/15")).to.equal(7)
+  })
 
 });
