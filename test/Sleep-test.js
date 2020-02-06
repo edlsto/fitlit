92e44605fadb2b57916879f9c5d22719a8a5af62
Lane Hunter <lanehunter.96@gmail.com>
2020-02-06 14:51:27 -0700

    Add findGoodSleepers test

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -21,10 +21,7 @@ describe('Sleep', function() {
     expect(sleep.averageHoursSleptPerDay(1)).to.equal(5.1);
   });
 
-  it('should calculate average sleep quality', function() {
-    const sleep = new Sleep(data);
-    expect(sleep.averageSleepQuality(1)).to.equal(2.9)
-  });
+  it('should calculate the sleepQuality')
 
   it('should return hours slept for a specific day', function() {
     const sleep = new Sleep(data);
@@ -40,6 +37,39 @@ describe('Sleep', function() {
   it('should return a user\'s sleep quality for a specific week', function() {
     const sleep = new Sleep(fullData);
     expect(sleep.sleepQualityForSpecificWeek(22,"2019/09/22")).to.deep.equal([ 4, 4.8, 4.7, 4.6, 1.4, 1.7, 3.2 ])
+  });
+
+  it('should calculate average sleep quality for a user', function() {
+    const sleep = new Sleep(data);
+    expect(sleep.averageSleepQualityForUser(1)).to.equal(2.9)
+  });
+
+  it('should find good sleepers for a specific week', function() {
+    const sleep = new Sleep(fullData);
+    expect(sleep.findGoodSleepers("2019/09/22")).to.deep.equal([
+  { userID: 5, averageSleepQuality: 3.5 },
+  { userID: 8, averageSleepQuality: 3.2 },
+  { userID: 13, averageSleepQuality: 3.7 },
+  { userID: 15, averageSleepQuality: 3.3 },
+  { userID: 19, averageSleepQuality: 3.2 },
+  { userID: 21, averageSleepQuality: 3.3 },
+  { userID: 22, averageSleepQuality: 3.5 },
+  { userID: 26, averageSleepQuality: 3.7 },
+  { userID: 28, averageSleepQuality: 3.1 },
+  { userID: 31, averageSleepQuality: 3.5 },
+  { userID: 32, averageSleepQuality: 3.3 },
+  { userID: 34, averageSleepQuality: 3.3 },
+  { userID: 35, averageSleepQuality: 3.4 },
+  { userID: 37, averageSleepQuality: 3.5 },
+  { userID: 39, averageSleepQuality: 3.3 },
+  { userID: 43, averageSleepQuality: 3.1 },
+  { userID: 45, averageSleepQuality: 3.3 },
+  { userID: 46, averageSleepQuality: 3.1 },
+  { userID: 47, averageSleepQuality: 3.3 },
+  { userID: 48, averageSleepQuality: 3.7 },
+  { userID: 49, averageSleepQuality: 3.1 },
+  { userID: 50, averageSleepQuality: 3.6 }
+])
   });
 
 });
