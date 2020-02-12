302c86831a1d727832db088265afc5864352eded
Edward Stoner <edwardlstoner@gmail.com>
2020-02-12 14:53:46 -0700

    Rework show friends leaderboard

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,7 @@
 let random = Math.floor(Math.random() * 50)
 let currentUser = new User(userData[random]);
 let hydration = new Hydration(hydrationData);
-let activity = new Activity(activityData, userData);
+let activity = new Activity(activityData);
 let sleep = new Sleep(sleepData);
 let userRepository = new UserRepository(userData);
 let today = '2019/08/15'
@@ -32,12 +32,32 @@ let trend = document.querySelector('#trend');
 let stepGoalTrend = document.querySelector('#step-goal-trend');
 let userFriends = document.querySelector("#friends");
 
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 username.innerText = currentUser.returnFirstName();
 userAddress.innerText = currentUser.address;
 userEmail.innerText = currentUser.email;
 userStrideLength.innerText = currentUser.strideLength;
 userStepGoal.innerText = numberWithCommas(currentUser.dailyStepGoal);
-let friends = activity.getFriendsLeaderboard(currentUser.id, today);
+let friendsList = currentUser.getFriendsAndSelf(currentUser, today);
+const result = friendsList.map(friend => {
+  return userRepository.data.find(person => {
+    return person.id === friend
+  })
+})
+
+const friends = result.map(friend => {
+  return {
+    name: friend.name,
+    numSteps: activity.getStatsForWeek(friend, today).map(el => el.numSteps).reduce((acc, el) => {
+      acc += el;
+      return acc;
+    }, 0)
+  }
+}).sort((a, b) => b.numSteps - a.numSteps)
+
 friends.forEach(el => {
   if (el.name === currentUser.name) {
     el.name = 'You'
@@ -50,65 +70,42 @@ friendList.forEach(friend => {
     friend.classList.add('bold')
   }
 })
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+
 averageStepGoal.innerText = numberWithCommas(userRepository.getAverageStepGoal());
 stepCompare.innerText = currentUser.dailyStepGoal > userRepository.getAverageStepGoal() ? 'higher' : 'lower';
 waterToday.innerText = hydration.waterConsumedForSpecificDay(currentUser.id, today)
 let waterWeek = hydration.waterConsumedForSpecificWeek(currentUser.id, today);
-console.log(waterWeek)
-// waterWeek.forEach(el => {
-//   waterWeekList.insertAdjacentHTML('afterbegin', `<li>${el} ounces</ul>`)
-// });
 
 sleepToday.innerText = sleep.getHoursSlept(currentUser.id, today);
 sleepQuality.innerText = sleep.getSleepQuality(currentUser.id, today);
 let sleepWeek = sleep.hoursSleptForSpecificWeek(currentUser.id, today);
-console.log(sleepWeek)
 let sleepQualityWeek = sleep.sleepQualityForSpecificWeek(currentUser.id, today)
-console.log(sleepQualityWeek)
-// sleepWeek.forEach((el, i) => {
-//   sleepWeekList.insertAdjacentHTML('afterbegin', `<li>${el} hours /  Quality score: ${sleepQualityWeek[i]}</ul>`)
-// });
-// sleepHoursAverage.innerText = sleep.averageHoursSleptPerDay(currentUser.id)
-// sleepQualityAverage.innerText = sleep.averageSleepQualityForUser(currentUser.id)
 
-stepsToday.innerText = numberWithCommas(activity.getSteps(currentUser.id, today))
+stepsToday.innerText = numberWithCommas(activity.getSteps(currentUser, today))
 
-activeToday.innerText = activity.getActiveMinutesForSpecificDay(currentUser.id, today)
+activeToday.innerText = activity.getActiveMinutesForSpecificDay(currentUser, today)
 
-flightsStairsToday.innerText = activity.getStairsClimbed(currentUser.id, today)
+flightsStairsToday.innerText = activity.getStairsClimbed(currentUser, today)
 
+stepsCompare.innerText = `${(Math.abs(activity.getSteps(currentUser, today) - activity.getAverageUserStats(today)[1]) / activity.getSteps(currentUser, today) * 100).toFixed(1)}% ${activity.getSteps(currentUser, today) > activity.getAverageUserStats(today)[1] ? 'higher' : 'lower'} than average`
 
-stepsCompare.innerText = `${(Math.abs(activity.getSteps(currentUser.id, today) - activity.getAverageUserStats(today)[1]) / activity.getSteps(currentUser.id, today) * 100).toFixed(1)}% ${activity.getSteps(currentUser.id, today) > activity.getAverageUserStats(today)[1] ? 'higher' : 'lower'} than average`
+minutesActiveCompare.innerText = `${(Math.abs(activity.getActiveMinutesForSpecificDay(currentUser, today) - activity.getAverageUserStats(today)[2]) / activity.getActiveMinutesForSpecificDay(currentUser, today) * 100).toFixed(1)}% ${activity.getActiveMinutesForSpecificDay(currentUser, today) > activity.getAverageUserStats(today)[2] ? 'higher' : 'lower'} than average`
 
+flightsStairsCompare.innerText = `${(Math.abs(activity.getStairsClimbed(currentUser, today) - activity.getAverageUserStats(today)[0]) / activity.getStairsClimbed(currentUser, today) * 100).toFixed(1)}% ${activity.getStairsClimbed(currentUser, today) > activity.getAverageUserStats(today)[0] ? 'higher' : 'lower'} than average`
 
-minutesActiveCompare.innerText = `${(Math.abs(activity.getActiveMinutesForSpecificDay(currentUser.id, today) - activity.getAverageUserStats(today)[2]) / activity.getActiveMinutesForSpecificDay(currentUser.id, today) * 100).toFixed(1)}% ${activity.getActiveMinutesForSpecificDay(currentUser.id, today) > activity.getAverageUserStats(today)[2] ? 'higher' : 'lower'} than average`
+let statsWeek = activity.getStatsForWeek(currentUser, today);
 
-
-flightsStairsCompare.innerText = `${(Math.abs(activity.getStairsClimbed(currentUser.id, today) - activity.getAverageUserStats(today)[0]) / activity.getStairsClimbed(currentUser.id, today) * 100).toFixed(1)}% ${activity.getStairsClimbed(currentUser.id, today) > activity.getAverageUserStats(today)[0] ? 'higher' : 'lower'} than average`
-
-let statsWeek = activity.getStatsForWeek(currentUser.id, today);
-// statsWeek.forEach(el => {
-//   stepsList.insertAdjacentHTML('afterbegin', `<li>${numberWithCommas(el[0])}</ul>`);
-//   minutesList.insertAdjacentHTML('afterbegin', `<li>${el[1]}</ul>`);
-//   stairsList.insertAdjacentHTML('afterbegin', `<li>${el[2]}</ul>`);
-// })
-
-
-const monthNames = ["Jan.", "Feb.", "March", "April", "May", "June",
-  "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."
+const monthNames = ["Jan.", "Feb.", "March", "April", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."
 ];
 
-let trendInfo = activity.getTrend(currentUser.id, today);
+let trendInfo = activity.getTrend(currentUser, today);
 if (trendInfo[0].date.toString() === new Date(today).toString()) {
   trend.innerText = `You are streaking! Your steps have increased for the last ${trendInfo.length} days!`
 } else {
   trend.innerText = 'The last time your daily steps increased each day for three or more days was ' + monthNames[trendInfo[0].date.getMonth()] + ' ' + trendInfo[0].date.getDate() + ', when you had a streak of ' + trendInfo.length + ' days.'
 }
 
-let stepTrendInfo = activity.findConsecutiveDaysReachedGoal(currentUser.id, today);
+let stepTrendInfo = activity.findConsecutiveDaysReachedGoal(currentUser, today);
 
 if (stepTrendInfo[0].date.toString() === new Date(today).toString()) {
   stepGoalTrend.innerText = `You have met you step goal for the last ${stepTrendInfo.length} days!`
@@ -116,14 +113,11 @@ if (stepTrendInfo[0].date.toString() === new Date(today).toString()) {
   stepGoalTrend.innerText = 'The last time you met your step goal 3 or more days in a row was ' + monthNames[stepTrendInfo[0].date.getMonth()] + ' ' + stepTrendInfo[0].date.getDate() + ', when you had a streak of ' + stepTrendInfo.length + ' days.'
 }
 
-
-var months = ['Jan.', 'Feb.', 'March', 'April', 'May', 'June', 'July', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.']
-
 var ctx = document.getElementById('weekChart').getContext('2d');
 var weekChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: statsWeek.map(el => months[new Date(el.date).getMonth()] + ' ' + new Date(el.date).getDate()),
+        labels: statsWeek.map(el => monthNames[new Date(el.date).getMonth()] + ' ' + new Date(el.date).getDate()),
         datasets: [{
             label: 'Steps',
             data: statsWeek.map(el => el.numSteps),
@@ -231,7 +225,7 @@ var ctx = document.getElementById('hydrationChart').getContext('2d');
 var hydrationChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: statsWeek.map(el => months[new Date(el.date).getMonth()] + ' ' + new Date(el.date).getDate()),
+        labels: statsWeek.map(el => monthNames[new Date(el.date).getMonth()] + ' ' + new Date(el.date).getDate()),
         datasets: [{
             label: 'Ounces of water',
             data: waterWeek,
@@ -279,7 +273,7 @@ var ctx = document.getElementById('sleepChart').getContext('2d');
 var sleepChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: statsWeek.map(el => months[new Date(el.date).getMonth()] + ' ' + new Date(el.date).getDate()),
+        labels: statsWeek.map(el => monthNames[new Date(el.date).getMonth()] + ' ' + new Date(el.date).getDate()),
         datasets: [{
             label: 'Hours of sleep',
             data: sleepWeek,
